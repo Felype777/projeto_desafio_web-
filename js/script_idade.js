0efc3dd2c400513e9e4d758c0997c9ae25ec7b53
Felype Gabryel <felype.g.ferreira@aluno.senai.br>
2026-06-12 14:57:32 -0300

    formatado o arquivo js/script_idade.js

--- js/script_idade.js
+++ js/script_idade.js
@@ -2,3 +2,8 @@
 const formDados = document.querySelector('#fromulario-pessoa')
 const divResultado = document.querySelector('#div-dados')
 
+//CAPTURANDO O EVENTO SUBMIT DO FORMULÁRIO
+formDados.addEventListener('submit' , (evt)=>{
+    evt.preventDefault()
+
+})
